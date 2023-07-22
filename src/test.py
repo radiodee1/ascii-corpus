3a769e7de84432af5e1a92aef71473e05beb36cd
David Liebman <radiodee1@gmail.com>
2023-07-22 11:45:19 -0400

    test.py

--- src/test.py
+++ src/test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from ascii_magic import AsciiArt
-from colorama import Back
+from ascii_magic import AsciiArt, Front, Back, from_image
+#from colorama import Back, Front
 
 
 #AsciiArt.quick_test()
 
-my_art = AsciiArt.from_image(path='../img/moon.jpg')
-my_art.to_terminal(columns=25, back=Back.BLACK)
-my_art.to_file(columns=75, path='../out/a.txt')
+my_art = AsciiArt.from_image(path='../img/moon.png')
+my_art.to_terminal(columns=75, monochrome=True)
+my_art.to_file(columns=75, monochrome=True , path='../out/a.txt')
