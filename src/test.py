05121f62bb7067879c99e2ccee672d86881be1ba
David Liebman <radiodee1@gmail.com>
2023-07-22 15:14:15 -0400

    trying out connect fn on button and menu item

--- src/test.py
+++ src/test.py
@@ -14,19 +14,34 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 
-class Handler:
-	def button_add_clicked(self, button):
-	    print("Hello GeeksForGeeks using Glade")
-
 builder = Gtk.Builder()
 builder.add_from_file("test.glade")
+
+
+
+class Handler():
+
+    def __init__(self):
+
+        self.button = builder.get_object("button-add")
+        self.button.connect('clicked', self.button_add_clicked)
+
+        self.exit = builder.get_object("menu-file-quit")
+        self.exit.connect("activate", self.menu_quit_clicked)
+
+    def button_add_clicked(self, button_in):
+        print("Hello Glade")
+        print(button_in)
+
+    def menu_quit_clicked(self, button_in):
+        print(button_in)
+        Gtk.main_quit()
+
 builder.connect_signals(Handler())
 
-#ournewbutton = builder.get_object("button1")
-#ournewbutton.set_label("Demo using Glade!")
 
 window = builder.get_object("corpus-top")
 
-window.connect("delete-event", Gtk.main_quit)
+window.connect("destroy", Gtk.main_quit)
 window.show_all()
 Gtk.main()
