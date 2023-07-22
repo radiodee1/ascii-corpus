56c42b649e2ce68b3c6d7671566f77adb26f7428
David Liebman <radiodee1@gmail.com>
2023-07-22 14:28:31 -0400

    glade file shows up on screen

--- src/test.py
+++ src/test.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
-from ascii_magic import AsciiArt, Front, Back, from_image
-#from colorama import Back, Front
+#from ascii_magic import AsciiArt, Front, Back, from_image
+
+#my_art = AsciiArt.from_image(path='../img/moon.png')
+#my_art.to_terminal(columns=75, monochrome=True)
+#my_art.to_file(columns=75, monochrome=True , path='../out/a.txt')
 
 
-#AsciiArt.quick_test()
 
-my_art = AsciiArt.from_image(path='../img/moon.png')
-my_art.to_terminal(columns=75, monochrome=True)
-my_art.to_file(columns=75, monochrome=True , path='../out/a.txt')
+import gi 
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+
+class Handler:
+	def button_add_clicked(self, button):
+	    print("Hello GeeksForGeeks using Glade")
+
+builder = Gtk.Builder()
+builder.add_from_file("test.glade")
+builder.connect_signals(Handler())
+
+#ournewbutton = builder.get_object("button1")
+#ournewbutton.set_label("Demo using Glade!")
+
+window = builder.get_object("corpus-top")
+
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+Gtk.main()
---
+++ virtualenv/do_make_package_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install -y python-gi-dev
+
+sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-4.0
+
+sudo apt install -y libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-4.0
