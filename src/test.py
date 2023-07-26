e064cf18152082977aa8767d82e2d06f0f3990da
David Liebman <radiodee1@gmail.com>
2023-07-26 11:08:08 -0400

    working on dots list -- no file write yet

--- src/test.py
+++ src/test.py
@@ -456,7 +456,7 @@ class Handler():
         options = []
         shuffle_list = []
         while list_done < self.dots_sizes[self.dots_sizes_number]:
-            if len(options) == 0:
+            if len(shuffle_list) == 0:
                 type_in = self.dots_types[self.dots_types_number]
                 start = int(type_in.split('-')[0])
                 end = int(type_in.split('-')[1]) + 1
@@ -467,7 +467,7 @@ class Handler():
                 pass
             if len(shuffle_list) > 0:
                 label = shuffle_list.pop(0)
-                label_string = self.dots_png_location + "/file_" + str(list_done) + "." + str(label) + ".png"
+                label_string = self.dots_png_location + "/file_dots_" + str(list_done) + "." + str(label) + ".png"
                 lines.append(label_string)
             list_done += 1 
         print(lines)
