a0ebf00b46a26bcfe1b51fe1ff99e01e1a1c34bc
David Liebman <radiodee1@gmail.com>
2023-07-30 17:44:11 -0400

    auto fill blocks in compose.py

--- src/compose.py
+++ src/compose.py
@@ -1,6 +1,8 @@
 # import the pygame module, so you can use it
 import pygame
 import sys
+
+import random
 # define a main function
 def main():
      
@@ -10,14 +12,45 @@ def main():
     #logo = pygame.image.load("logo32x32.png")
     #pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
-     
+    
+    square_size = 30 
+    size_x = 340 
+    size_y = 280 
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((340,280))
+    screen = pygame.display.set_mode((size_x,size_y))
     screen.fill('white') 
     # define a variable to control the main loop
     running = True
     
     objects = []
+
+    tot = int(sys.argv[1].split(".")[-2])
+    
+    spot = 0 
+    if len(sys.argv) > 2:
+        print(sys.argv[2], tot)
+        while len(objects) < tot and spot < 2000:
+            x = random.randint(0 + square_size , size_x - square_size )
+            y = random.randint(0 + square_size , size_y - square_size )
+            if len(objects) == 0:
+                objects.append((x,y))
+            #box = (x,y)
+            keep = True
+            for prev in objects:
+                p_x = prev[0]
+                p_y = prev[1]
+                square_size_plus = square_size * 2
+
+                span_x = [ i for i in range(p_x - square_size_plus , p_x + square_size_plus ) ]
+                span_y = [ i for i in range(p_y - square_size_plus , p_y + square_size_plus ) ]
+                #print(span_x)
+                if x in span_x and y in span_y:
+                    keep = False
+                    #objects.append((x,y))
+            if keep:
+                objects.append((x,y))
+            spot += 1 
+        pass 
 
     # main loop
     while running:
@@ -38,10 +71,10 @@ def main():
 
             for i in objects:
                 down1, down2 = i 
-                down1 -= 15
-                down2 -= 15
-                down3 = 30
-                down4 = 30
+                down1 -= square_size / 2 
+                down2 -= square_size / 2
+                down3 = square_size
+                down4 = square_size
                 pygame.draw.rect(screen, 'red',  [down1, down2, down3, down4] )
                 #print(i, 'red')
 
@@ -49,13 +82,13 @@ def main():
         pygame.display.flip() 
     
     print(sys.argv)
-    num = sys.argv[-1].split(".")[-2]
+    num = sys.argv[1].split(".")[-2]
 
     if len(objects) > 0 or num.startswith('0'):
         print(num)
         name = '../../test.png'
-        if len(sys.argv) == 2:
-            name = sys.argv[-1]
+        if len(sys.argv) >= 2:
+            name = sys.argv[1]
             print(name)
         pygame.image.save(screen, name)
 
