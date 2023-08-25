e471d23e28d46f0a75be70086685e2adbe656f0a
David Liebman <radiodee1@gmail.com>
2023-08-25 13:10:08 -0400

    rm commented intro lines

--- src/test.py
+++ src/test.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-#from ascii_magic import AsciiArt, Front, Back, from_image
-
-#my_art = AsciiArt.from_image(path='../img/moon.png')
-#my_art.to_terminal(columns=75, monochrome=True)
-#my_art.to_file(columns=75, monochrome=True , path='../out/a.txt')
-
-
 
 import gi 
 
